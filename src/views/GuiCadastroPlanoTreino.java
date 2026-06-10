c1cbcf23375cbcbe3193dfe3e70c67b5f06c297c
Gabriel Liz <201018789+GabrielLiz132@users.noreply.github.com>
2026-06-09 21:51:10 -0300

    Criação das Classes Gui do Grabriel e alteração da UML na parte dos GUIs

---
+++ src/views/GuiCadastroPlanoTreino.java
@@ -0,0 +1,12 @@
+package views;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class GuiCadastroPlanoTreino extends JFrame{
+	public GuiCadastroPlanoTreino() {
+	}
+	
+}
