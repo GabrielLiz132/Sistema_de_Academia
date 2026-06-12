0ae54ef3a88cdda2fd05dbb1bc83d2b2282a53cd
Juan <juanruan659@gmail.com>
2026-06-12 17:49:27 -0300

    criação da logica da consulta de alunos

--- src/views/GuiCadastroPlanoTreino.java
+++ src/views/GuiCadastroPlanoTreino.java
@@ -2,7 +2,6 @@ package views;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
